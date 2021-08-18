d15d455b1d1d2a8322edad69d3e9457cabdd33b9
A267 <77297903+A267-dev@users.noreply.github.com>
2021-08-18 16:43:36 -0700

    Fourth Gen Blam Update

---
+++ src/Blamite/Blam/FourthGen/Structures/FourthGenTagGroup.cs
@@ -0,0 +1,25 @@
+﻿using Blamite.Serialization;
+
+namespace Blamite.Blam.FourthGen.Structures
+{
+	public class FourthGenTagGroup : ITagGroup
+	{
+		public FourthGenTagGroup(StructureValueCollection values)
+		{
+			Load(values);
+		}
+
+		public int Magic { get; set; }
+		public int ParentMagic { get; set; }
+		public int GrandparentMagic { get; set; }
+		public StringID Description { get; set; }
+
+		private void Load(StructureValueCollection values)
+		{
+			Magic = (int) values.GetInteger("magic");
+			ParentMagic = (int) values.GetInteger("parent magic");
+			GrandparentMagic = (int) values.GetInteger("grandparent magic");
+			Description = new StringID(values.GetIntegerOrDefault("stringid", 0));
+		}
+	}
+}
