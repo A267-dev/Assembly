81e8b0569472ef7b4caf85bcbdd290cfec94edfa
A267 <77297903+A267-dev@users.noreply.github.com>
2021-08-19 19:28:10 -0700

    Bug Fixes

--- src/Blamite/RTE/FourthGen/FourthGenRTEProvider.cs
+++ src/Blamite/RTE/FourthGen/FourthGenRTEProvider.cs
@@ -17,101 +17,51 @@ namespace Blamite.RTE.Eldorado
 	/// </summary>
 	public class EldoradoRTEProvider : IRTEProvider
 	{
-		// TODO: Maybe find a way to dynamically get these addresses so we can support other versions of the game
-		public static uint MaxTagCountAddress;
-		public static uint TagIndexArrayPointerAddress;
-		public static uint TagAddressArrayPointerAddress;
-
 		/// <summary>
-		/// Initializes a new instance of the <see cref="EldoradoRTEProvider"/> class.
+		///     Constructs a new ThirdGenMCCRTEProvider.
 		/// </summary>
-		/// <param name="exeName">The name of the executable to connect to.</param>
-		public EldoradoRTEProvider(string exeName)
-		{
-			EXEName = exeName;
-
-			MaxTagCountAddress = 0x22AB008;
-			TagIndexArrayPointerAddress = 0x22AAFFC;
-			TagAddressArrayPointerAddress = 0x22AAFF8;
-		}
-
-		/// <summary>
-		///     The type of connection that the provider will establish.
-		///     Always RTEConnectionType.LocalProcess.
-		/// </summary>
-		public RTEConnectionType ConnectionType
-		{
-			get { return RTEConnectionType.LocalProcess; }
-		}
-
-		/// <summary>
-		/// Gets or sets the name of the executable to connect to.
-		/// </summary>
-		public string EXEName { get; set; }
+		public EldoradoRTEProvider(EngineDescription engine) : base(engine)
+		{ }
 
 		/// <summary>
 		///     Obtains a stream which can be used to read and write a cache file's meta in realtime.
 		///     The stream will be set up such that offsets in the stream correspond to meta pointers in the cache file.
 		/// </summary>
 		/// <param name="cacheFile">The cache file to get a stream for.</param>
-		/// <returns>
-		/// The stream if it was opened successfully, or null otherwise.
-		/// </returns>
-		public IStream GetMetaStream(ICacheFile cacheFile = null)
+		/// <returns>The stream if it was opened successfully, or null otherwise.</returns>
+		public override IStream GetMetaStream(ICacheFile cacheFile = null)
 		{
 			if (!CheckBuildInfo())
 				return null;
 
-			// Open a handle to the game process
-			var gameProcess = FindGameProcess();
+			Process gameProcess = FindGameProcess();
 			if (gameProcess == null)
 				return null;
-			var memoryStream = new ProcessMemoryStream(gameProcess);
 
-			var exeBase = (uint)memoryStream.BaseProcess.MainModule.BaseAddress - 0x400000;
+			PokingInformation info = RetrieveInformation(gameProcess);
 
-			var tagAddress = GetTagAddress(new EndianReader(memoryStream, Endian.LittleEndian), tag.Index.Index, exeBase);
-			if (tagAddress == 0)
+			if (!info.HeaderPointer.HasValue && (!info.HeaderAddress.HasValue || !info.MagicAddress.HasValue))
+				throw new NotImplementedException("Poking information is missing required values.");
+
+			var gameMemory = new ProcessModuleMemoryStream(gameProcess, _buildInfo.GameModule);
+			var mapInfo = new ThirdGenMapPointerReader(gameMemory, _buildInfo, info);
+
+			long metaMagic = mapInfo.CurrentCacheAddress;
+
+			if (gameMemory.BaseModule == null)
+				return null;
+
+			if (cacheFile != null && mapInfo.MapName != cacheFile.InternalName)
 			{
-				memoryStream.Close();
+				gameMemory.Close();
 				return null;
 			}
-			var offsetStream = new OffsetStream(memoryStream, tagAddress - tag.HeaderLocation.AsOffset());
-			return new EndianStream(offsetStream, Endian.LittleEndian);
-		}
+			
+			if (metaMagic == 0)
+				return null;
 
-		private Process FindGameProcess()
-		{
-			var processes = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(EXEName));
-			return processes.Length > 0 ? processes[0] : null;
-		}
-
-		public static uint GetTagAddress(IReader memoryReader, ushort index, uint exeBase)
-		{
-			//uint newcountaddr = MaxTagCountAddress;
-			// Read the tag count and validate the tag index
-			memoryReader.SeekTo(MaxTagCountAddress + exeBase);
-			var maxIndex = memoryReader.ReadUInt16();
-			if (index >= maxIndex)
-				return 0;
-
-			// Read the tag index table to get the index of the tag in the address table
-			memoryReader.SeekTo(TagIndexArrayPointerAddress + exeBase);
-			var tagIndexTableAddress = memoryReader.ReadUInt32();
-			if (tagIndexTableAddress == 0)
-				return 0;
-			memoryReader.SeekTo(tagIndexTableAddress + index * 4);
-			var addressIndex = memoryReader.ReadInt32();
-			if (addressIndex < 0)
-				return 0;
-
-			// Read the tag's address in the address table
-			memoryReader.SeekTo(TagAddressArrayPointerAddress + exeBase);
-			var tagAddressTableAddress = memoryReader.ReadUInt32();
-			if (tagAddressTableAddress == 0)
-				return 0;
-			memoryReader.SeekTo(tagAddressTableAddress + addressIndex * 4);
-			return memoryReader.ReadUInt32();
+			var metaStream = new OffsetStream(gameMemory, metaMagic);
+			return new EndianStream(metaStream, BitConverter.IsLittleEndian ? Endian.LittleEndian : Endian.BigEndian);
 		}
 	}
 
